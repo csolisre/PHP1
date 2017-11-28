49386317ca0114a71fa9bd3e0236f8506bbb9caf
Carlos Solis <csolisre@gmail.com>
2017-11-28 13:13:08 +0100

    for and while lust

---
+++ invoerform.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style>
+            h5 {
+                color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <form action="index.php" method="get">
+            Vul je naam in: <input type="text" name="naam">
+            <input type="submit" value="ok">
+        </form>
+    </body>
+</html>
